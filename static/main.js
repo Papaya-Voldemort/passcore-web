12ed8bcf80e2f6b4cec118cac419b5a71a473b00
Eli Nelson <eli.n992@stu.nebo.edu>
2026-03-18 13:40:19 -0600

    Update passcore version to 0.2.2; enhance password scoring UI with strength bar

--- static/main.js
+++ static/main.js
@@ -1,4 +1,21 @@
 const form = document.querySelector("#password-form");
+const bar = document.querySelector("#strength-bar");
+
+const colors = {
+    "F": "#f82b2b",
+    "D-": "#ff6f29",
+    "D": "#e47d4a",
+    "D+": "#ffc924",
+    "C-": "#ffd033",
+    "C": "#e4bf4f",
+    "C+": "#aaf829",
+    "B-": "#a7f12d",
+    "B": "#8fc635",
+    "B+": "#15e461",
+    "A-": "#1fd662",
+    "A": "#22c55e",
+    "A+": "#26b55a"
+}
 
 form.addEventListener("submit", async (e) => {
     const password = document.querySelector("#password").value;
@@ -18,6 +35,11 @@ form.addEventListener("submit", async (e) => {
 
             document.querySelector("#results").textContent =
                 `Score: ${data.score} | Grade ${data.grade}`;
+
+            bar.style.width = `${data.score}%`;
+            bar.style.background = colors[data.grade] || "gray";
+
+
         } catch (err) {
             console.error(err);
             document.querySelector("#results").textContent = "Error calling API";
